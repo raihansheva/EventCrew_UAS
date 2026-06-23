e237c8d68cc4ffea32d8da7c2262f4f1320f9e6c
raihansheva <shevareva22@gmail.com>
2026-06-23 21:45:44 +0700

    create layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EventCrew</title>
+    <link rel="stylesheet" href="{{ asset('style/layout/main.css') }}">
+</head>
+
+<body>
+    <div class="wrapper">
+        <nav>
+            <div class="container">
+                <div class="navbar-brand">
+                    <h1 class="brand">EventCrew</h1>
+                </div>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="#">Events</a>
+                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="#">Contact</a>
+                    <div class="area-button-login">
+                        <div class="button-login">
+                            <span>Login</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            <p>&copy; 2024 EventCrew. All rights reserved.</p>
+        </footer>
+    </div>
+</body>
+
+</html>
