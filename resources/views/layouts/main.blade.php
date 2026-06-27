269f108dd270dc60e38d61da8000d10f33654a21
raihansheva <shevareva22@gmail.com>
2026-06-27 22:04:59 +0700

    perubahan layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,17 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>EventCrew</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    
     <link rel="stylesheet" href="{{ asset('style/layout/main.css') }}">
 </head>
 
 <body>
     <div class="wrapper">
         <nav>
-            <div class="container">
+            <div class="navbar-container">
                 <div class="navbar-brand">
                     <h1 class="brand">Event<span class="brand-highlight">Crew</span></h1>
                 </div>
-                <div class="navbar-nav">
+                <div class="menu-nav">
                     <a class="nav-link" href="{{ url('/') }}">Home</a>
                     <a class="nav-link" href="{{ url('/event') }}">Events</a>
                     <a class="nav-link" href="{{ url('/about') }}">About</a>
@@ -37,6 +41,12 @@
             <p>&copy; 2024 EventCrew. All rights reserved.</p>
         </footer>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
+        integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
