0e900bf5ba91fa637bde736218f61280b57aff76
raihansheva <shevareva22@gmail.com>
2026-07-08 19:19:36 +0700

    revisi navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,10 +8,10 @@
     <title>EventCrew</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 
-    
+
     <link rel="stylesheet" href="{{ asset('style/layout/main.css') }}">
 </head>
 
@@ -23,47 +23,46 @@
                     <h1 class="brand">Event<span class="brand-highlight">Crew</span></h1>
                 </div>
                 <div class="menu-nav">
-                <div class="menu-nav">
-                    <a class="nav-link" href="{{ url('/') }}">Home</a>
-                    <a class="nav-link" href="{{ url('/event') }}">Events</a>
-                    <a class="nav-link" href="{{ url('/about') }}">About</a>
-                    <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
-                    <a class="nav-link" href="{{ url('/pendaftaran') }}">
-                        Pendaftaran
-                    </a>
-                    @guest
-                        <div class="area-button-login">
-                            <div class="button-login" data-bs-toggle="modal" data-bs-target="#exampleModalLogin">
-                                <p>Login</p>
-                            </div>
-                        </div>
-                    @endguest
-                    @auth
-                        <a class="nav-link dropdown-toggle text-center" href="#" data-bs-toggle="dropdown">
-                            Profile
-                        </a>
-                        <ul class="dropdown-menu">
-                            <div class="area-menu-drop">
-                                <div class="area-link">
-                                    <div class="area-setting">
-                                        <a class="dropdown-item" href="#">
-                                            Akun Saya
-                                        </a>
-                                    </div>
-                                    <div class="area-logout">
-                                        <form action="{{ route('logout') }}" method="POST">
-                                            @csrf
-                                            <button class="dropdown-item text-danger" type="submit">
-                                                Logout
-                                            </button>
-                                        </form>
-                                    </div>
+                    <div class="menu-nav">
+                        <a class="nav-link" href="{{ url('/') }}">Home</a>
+                        <a class="nav-link" href="{{ url('/event') }}">Events</a>
+                        <a class="nav-link" href="{{ url('/about') }}">About</a>
+                        <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
+                        @guest
+                            <div class="area-button-login">
+                                <div class="button-login" data-bs-toggle="modal" data-bs-target="#exampleModalLogin">
+                                    <p>Login</p>
                                 </div>
                             </div>
-                        </ul>
-                    @endauth
+                        @endguest
+                        @auth
+                            @if (Auth::user()->role == 'volunteer')
+                                <a class="nav-link dropdown-toggle text-center" href="#" data-bs-toggle="dropdown">
+                                    Profile
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <div class="area-menu-drop">
+                                        <div class="area-link">
+                                            <div class="area-setting">
+                                                <a class="dropdown-item" href="#">
+                                                    Akun Saya
+                                                </a>
+                                            </div>
+                                            <div class="area-logout">
+                                                <form action="{{ route('logout') }}" method="POST">
+                                                    @csrf
+                                                    <button class="dropdown-item text-danger" type="submit">
+                                                        Logout
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </ul>
+                            @endif
+                        @endauth
+                    </div>
                 </div>
-            </div>
         </nav>
         <main>
             @yield('content')
@@ -301,7 +300,8 @@
                                     <p class="textNotif">{{ $message }}</p>
                                 </div>
                             @enderror
-                            <form class="form-register" action="{{ route('register.post.panitia') }}" method="post">
+                            <form class="form-register" action="{{ route('register.post.panitia') }}"
+                                method="post">
                                 @csrf
                                 <div class="content-regis">
                                     <div class="area-regis-kiri">
